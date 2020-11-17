aed81c03b1dd13390f9d7b9b31b16ec2d28782fd
Pranshu1902 <70687348+Pranshu1902@users.noreply.github.com>
2020-11-18 01:55:12 +0530

    Add files via upload

---
+++ Random_password.py
@@ -0,0 +1,15 @@
+import random
+
+all_characters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'M', 'N', 'O', 'p', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>','*', '(', ')']
+
+x = int(input("Enter the length of the password "))
+
+password = []
+
+for i in range(x):
+    a = random.choice(all_characters)
+    all_characters.remove(a)
+    password.append(a)
+
+print("".join(password))
+    
